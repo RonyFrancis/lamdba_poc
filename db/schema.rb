4e63aa24139e0ff65860d398553f69e070a0daab
Rony Francis <rony@sapaad.com>
2018-10-08 16:11:46 +0530

    schema added

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_10_05_094635) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "lamdbas", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
