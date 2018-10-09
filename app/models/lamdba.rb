adecd5daa193b6285b1bbee5bd27ff2c531e1b64
Rony Francis <rony@sapaad.com>
2018-10-09 09:59:39 +0530

    update query for lambda

--- app/models/lamdba.rb
+++ app/models/lamdba.rb
@@ -2,17 +2,21 @@ class Lamdba < ApplicationRecord
   require 'rest-client'
   def details(params)
     # RestClient.post ENV['url'], key: 1
+    @lamdba = Lamdba.new
+    @lamdba.key = response['key'].to_i
+    @lamdba.save
     @response = RestClient::Request.execute(method: :post, url: ENV['url'],
                                             payload: {
-                                              key: params['key'].to_i
+                                              key: params['key'].to_i,
+                                              id: @lamdba.id
                                             }.to_json,
                                             timeout: 10)
 
-    # response = JSON.parse(@response.body)
+    response = JSON.parse(@response.body)
     # @lamdba = Lamdba.new
     # @lamdba.key = response['key'].to_i
     # @lamdba.square_value = response['square_value'].to_i
     # @lamdba.save
-    Lamdba.last.id
+    response['key'].to_i
   end
 end
