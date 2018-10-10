845b752109ee00ee5e4d900fe4b050c8a1bda32d
Rony Francis <rony@sapaad.com>
2018-10-10 16:32:04 +0530

    MAde post method

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :lamdba, only: %i[index create new show]
-  get '/welcome', to: 'lamdba#welcome'
+  post '/welcome', to: 'lamdba#welcome'
   root to: 'lamdba#new'
 end
