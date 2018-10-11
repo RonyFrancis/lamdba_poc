72e67620e36503e1021058f85caa4d1899ba401a
Rony Francis <ronydfrnd@gmail.com>
2018-10-11 14:59:34 +0530

    rest_client

--- app/controllers/lamdba_controller.rb
+++ app/controllers/lamdba_controller.rb
@@ -1,6 +1,7 @@
 # LamdbaController to communicate with lamdba function
 class LamdbaController < ApplicationController
   skip_before_action :verify_authenticity_token
+  require 'openssl'
   def create
     id = Lamdba.new.details(params['lamdba'])
     redirect_to lamdba_path(id)
@@ -19,8 +20,27 @@ class LamdbaController < ApplicationController
   end
 
   def welcome
-    request.headers.each { |key, value| Rails.logger.info %(#{key} : #{value}) }
-    Rails.logger.info params
-    render json: { msg: 'welcome' }
+    # request.headers.each { |key, value| Rails.logger.info %(#{key} : #{value}) }
+    Rails.logger.info 'a' * 10
+    Rails.logger.info params.inspect
+    Rails.logger.info 'a' * 10
+    # webhook_secret = '8a10a0d9-3366-4486-a945-36c804572373'
+    # sequence_guid = request.headers["HTTP_X_DELIVEROO_SEQUENCE_GUID"]
+    #  payload = parms
+    # digest = OpenSSL::Digest.new('sha256')
+    # data = "#{sequence_guid} \n #{payload}"
+    # auth = OpenSSL::HMAC.hexdigest(digest, webhook_secret, data)
+
+    res = RestClient::Request.execute(method: :post, url: 'https://developers.net/v1/orders/820639632508-5729/sync_status',
+                                      user: 'SAPAAD_PTE_LTD_Test--0075e1ef-4fa4-46b5-8ee3-bb1cc243bb3f',
+                                      password: '42c11fb4-d895-4529-9c3f-7de6332ef9a9',
+                                      payload: {
+                                        "occurred_at": Time.now,
+                                        "status": 'failed',
+                                        "reason": 'invalid_total_price',
+                                        "notes": ''
+                                      })
+    Rails.logger.info res.body
+    render json: { msg: auth }
   end
 end
