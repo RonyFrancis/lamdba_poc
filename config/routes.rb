cab15bf85969106af16d77338dec6a8db2ad2486
Rony Francis <ronydfrnd@gmail.com>
2018-10-15 11:11:44 +0530

    deliveroo endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :lamdba, only: %i[index create new show]
   post '/welcome', to: 'lamdba#welcome'
+  post '/deliveroo', to: 'lamdba#welcome'
   root to: 'lamdba#new'
 end
